006e2c60a08262fe1e7a2e85a3798fb52d8242f1
Sil <sil@matise.nl>
2018-01-03 11:27:06 +0100

    adding settings

---
+++ src/settings/index.js
@@ -0,0 +1,17 @@
+import button from './button.json';
+import colors from './colors.json';
+import elements from './elements.json';
+import grid from './grid.json';
+import navigation from './navigation.json';
+import settings from './settings.json';
+import typography from './typography.json';
+
+export default {
+	button,
+	colors,
+	elements,
+	grid,
+	navigation,
+	settings,
+	typography
+};
