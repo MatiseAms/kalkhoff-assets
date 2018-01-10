eefbf0b28895f6c8f8b913a16a9388be5de8a4b9
Sil <sil@matise.nl>
2018-01-10 11:42:28 +0100

    cleanup, working less, empty assets output, filled assets-classes

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,5 +1,4 @@
 module.exports = {
-
 	less: {
 		files: [{
 			expand: true,
--- scripts/less.php
+++ scripts/less.php
@@ -2,8 +2,26 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
+$output_folder = '/../src/test/less/';
+// create test distFolder
+
+// if (!file_exists($output_folder)) {
+//   mkdir($output_folder, 0775, true);
+// }
+
+
+
+// Base Assets
 $parser = new Less_Parser();
 $parser->parseFile( __DIR__ . '/../dist/assets.less', 'http://example.com/' );
 $css = $parser->getCss();
 
-file_put_contents(__DIR__ . '/../dist/css/assets.css', $css);
+file_put_contents(__DIR__ . $output_folder.'assets.css', $css);
+
+// Classes Assets
+$parser_classes = new Less_Parser();
+$parser_classes->parseFile( __DIR__ . '/../dist/assets-classes.less', 'http://example.com/' );
+$css_classes = $parser_classes->getCss();
+
+file_put_contents(__DIR__ . $output_folder.'assets-classes.css', $css_classes);
