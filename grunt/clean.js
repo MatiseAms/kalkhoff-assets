7416d0d98575d0db97f17c7214b1904bf4ff418b
Stephan Hoogland <stephan@shoogland.com>
2018-01-11 10:06:13 +0100

    added grunt-newer package

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,6 +1,16 @@
 module.exports = {
 	build: [
-		'<%= config.dist.root %>'
+		'<%= config.dist.root %>images/',
+		'<%= config.dist.root %>less/*/',
+		'!<%= config.dist.root %>less/icons/',
+		'<%= config.dist.root %>scss/*/',
+		'!<%= config.dist.root %>scss/icons/',
+		'<%= config.dist.root %>settings/',
+		'<%= config.dist.root %>*.less',
+		'<%= config.dist.root %>*.scss'
+	],
+	magento: [
+		'<%= config.magento.component %>'
 	],
 	test: [
 		'<%= config.test.root %>'
